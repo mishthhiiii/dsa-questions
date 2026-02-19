6ca5bd27996c6e7d9b1a302d34b74f4dd7a7b28c
Mishthi Chaurasia <2k23.ece2311300@gmail.com>
2026-02-20 01:17:49 +0530

    Add files via upload

---
+++ lecture_5/forsum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "value of n:" << n ;
+
+    int sum = 0;
+    for(int i=1; i<=n ; i++){
+    // sum = sum + i;
+    sum += i ;
+    }
+    cout << sum << endl;
+
+   
+    return 0;
+}
+
---
+++ lecture_5/learn.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// using namespace std;  if we give this we do not nee dto give std everywhere 
+int main(){
+     std::cout<<"hello mishthi"<<"\n";// <<"\n" or <<std::endl;  both take to the next line but \n is fast so most people use \n 
+     std::cout<<"hello mishthi"<<"\n";
+ 
+std::cout<<"hello mishthi"<<std::endl<<"how are you"<<std::endl;
+return 0;
+}
+
